adbec9ebbf9f9a83652b60db4a402ab32bf06817
Shrestha36 <86195716+Shrestha36@users.noreply.github.com>
2026-01-31 16:59:13 +0530

    adding route to auth.route.js

--- HiSplit/backend/routes/auth.routes.js
+++ HiSplit/backend/routes/auth.routes.js
@@ -3,6 +3,10 @@ import { loginUser } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.send("Login route is working. Use POST to login.");
+});
+
 // POST /api/login
 router.post("/", loginUser);
 
